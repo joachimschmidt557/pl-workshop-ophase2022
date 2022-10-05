5d48f57a0636f072c745a89f05bba73570aefa58
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-10-05 23:18:32 +0200

    Initial commit: initialize nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            tectonic
+            (python3.withPackages (ps: with ps; [ pygments ]))
+          ];
+        };
+      }
+    );
+
+}
